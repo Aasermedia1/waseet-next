931029bb59e3dc02fc7c5daa9074be8ceef4287b
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-06 10:49:07 +0200

    Delete pages/pages/index.js

--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', paddingTop: '100px' }}>
-      <h1>✅ Waseet is live!</h1>
-      <p>Next.js + Vercel setup is working 🎉</p>
-    </div>
-  );
-}
