f69ddedb5b5a28f376f54cb66c0de5169e34387d
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-07 23:25:39 +0200

    Create BuiltByAaser.js

---
+++ components/BuiltByAaser.js
@@ -0,0 +1,11 @@
+// components/BuiltByAaser.js
+
+export default function BuiltByAaser() {
+  return (
+    <section className="text-center py-6 text-sm text-gray-500">
+      <p>
+        وسيط منتج من <strong>AaserMedia</strong> © 2026
+      </p>
+    </section>
+  );
+}
