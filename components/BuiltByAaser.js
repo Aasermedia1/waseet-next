708c4f157697f180ca30b3715eb38378003e8159
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 01:21:19 +0200

    Update BuiltByAaser.js

--- components/BuiltByAaser.js
+++ components/BuiltByAaser.js
@@ -1,11 +1,1 @@
-// components/BuiltByAaser.js
 
-export default function BuiltByAaser() {
-  return (
-    <section className="text-center py-6 text-sm text-gray-500">
-      <p>
-        وسيط منتج من <strong>AaserMedia</strong> © 2026
-      </p>
-    </section>
-  );
-}
