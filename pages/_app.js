9a28f77b719f413583b300d3af554de327d020bc
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 01:34:58 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,7 @@
-import Head from "next/head";
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* Load Google Fonts */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&family=Tajawal:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
