a596af4215210f2453ce1dd14d1367440f148580
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-07 17:58:17 +0200

    Update _app.js

--- pages/pages/_app.js
+++ pages/pages/_app.js
@@ -2,5 +2,15 @@ import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
