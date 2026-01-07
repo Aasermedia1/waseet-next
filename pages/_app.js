34b728bf5d6932519726540f1a1f162e6c94cf98
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-07 20:41:54 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
-import Head from 'next/head';
-import '../styles/globals.css';
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Load Google Fonts */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&family=Tajawal:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
