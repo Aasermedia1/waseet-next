db9b7482787fa01c2919e809e5a3623a555a9704
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-07 21:42:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
+import Navbar from '../components/Navbar';
 import styles from '../styles/Home.module.css'; // make sure this file exists
 import ChatDemo from '../components/ChatDemo'; // this is the animated widget
 
