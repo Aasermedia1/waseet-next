29434b29847bcb0fbaf5297be66665d80aeae95f
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 12:19:24 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,20 @@
 import '../styles/globals.css';
-import Head from "next/head";
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Fonts */}
+        {/* ✅ Load Cairo and Tajawal from Google Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Cairo:wght@500;700;800&family=Tajawal:wght@400;500;700&display=swap"
           rel="stylesheet"
         />
+        <style>{`
+          body {
+            font-family: 'Tajawal', 'Cairo', sans-serif;
+          }
+        `}</style>
       </Head>
       <Component {...pageProps} />
     </>
