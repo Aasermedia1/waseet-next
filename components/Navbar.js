7f7c9a02dd44350a549856b88d06387d13da081c
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 10:56:40 +0200

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,1 @@
-export default function Navbar() {
-  return (
-    <header className="w-full py-4 bg-white shadow-md">
-      <nav className="container mx-auto px-4 flex justify-between items-center">
-        {/* Logo and links will go here */}
-        <span className="text-xl font-bold">وسيط</span>
-      </nav>
-    </header>
-  );
-}
+
