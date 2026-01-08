b142fca4442d6be121e0fa6543241257c8305c71
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 14:19:33 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import ProblemsSection from "../components/ProblemsSection";
 
 export default function Home() {
   return (
