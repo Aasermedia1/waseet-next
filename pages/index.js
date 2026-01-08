5440892341f62b1aac88653f571268ea08f11e7a
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 20:27:14 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,19 @@ import Hero from "../components/Hero";
 import ProblemsSection from "../components/ProblemsSection";
 import FeaturesSection from "../components/FeaturesSection";
 import SuccessStories from "../components/SuccessStories";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ProblemsSection />
+      <FeaturesSection />
+      <SuccessStories />
+      {/* Next sections… */}
+    </>
+  );
+}
 import TrustSection from "../components/TrustSection";
 
 export default function Home() {
