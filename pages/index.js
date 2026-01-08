7c0f27c8b201d9de3911cf6a58cf012a0bb93c46
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-08 11:07:34 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center text-gray-700 text-xl">
-      {/* 🧼 Waseet landing page will be built here from scratch */}
-      وسيط — صفحة نظيفة وجاهزة للبناء من جديد
+      {/* 🌸 Waseet landing page will be built here from scratch */}
+      <p>وسيط — صفحة نظيفة وجاهزة للبناء من جديد</p>
     </div>
   );
 }
