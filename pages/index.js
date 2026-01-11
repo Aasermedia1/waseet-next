207d4dd5a281ad09ac56ed45d2f74d8d0f546459
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-11 21:48:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,45 +2,30 @@ import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import ProblemsSection from "../components/ProblemsSection"
 import FeaturesSection from "../components/FeaturesSection"
-import ClientsReviews from "../components/ClientsReviews";
-
-export default function Home() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <ProblemsSection />
-      <FeaturesSection />
-      <TrustSection />
-
-      {/* Clients Reviews Carousel */}
-      <ClientsReviews />
-
-      <SuccessStories />
-    </>
-  );
-}
-
 import TrustSection from "../components/TrustSection"
+import ClientsReviews from "../components/ClientsReviews"
 import SuccessStories from "../components/SuccessStories"
 
 export default function Home() {
   return (
     <>
-      {/* Navbar */}
+      {/* Navigation Bar */}
       <Navbar />
 
       {/* Hero Section */}
       <Hero />
 
-      {/* Problems / Challenges Section */}
+      {/* Problems Section */}
       <ProblemsSection />
 
-      {/* Features: Accordion + Image */}
+      {/* Features Section */}
       <FeaturesSection />
 
-      {/* Trust Section: ليه تثق في وسيط */}
+      {/* Trust Section */}
       <TrustSection />
+
+      {/* Clients Reviews Carousel */}
+      <ClientsReviews />
 
       {/* Success Stories / Stats */}
       <SuccessStories />
