f338cb762527f946e8802dad86e7d23481ca8c75
Aasermedia1 <99656941+Aasermedia1@users.noreply.github.com>
2026-01-15 19:11:58 +0200

    Create how-it-works.js

---
+++ pages/how-it-works.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+import HowItWorksHero from "../components/HowItWorksHero";
+import Footer from "../components/Footer";
+
+export default function HowItWorks() {
+  return (
+    <>
+      <Navbar />
+      <HowItWorksHero />
+      {/* Next sections below */}
+      <Footer />
+    </>
+  );
+}
